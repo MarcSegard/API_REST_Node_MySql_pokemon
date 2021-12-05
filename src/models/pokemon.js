86bbb73b1dd6c968a2bdcf23565e2b85302dbb73
marcsegard <marcsegard@free.fr>
2021-12-05 15:55:15 +0100

    Ajout validateur simple sequelize

--- src/models/pokemon.js
+++ src/models/pokemon.js
@@ -10,18 +10,36 @@ module.exports = (sequelize, DataTypes) => {
       name: {
         type: DataTypes.STRING,
         allowNull: false,
+        validate: {
+          notEmpty: {
+            msg: "Le nom du pokemon ne peut pas être une chaine vide.",
+          },
+          notNull: { msg: "Le nom du pokemon est une propriété requise." },
+        },
       },
       hp: {
         type: DataTypes.INTEGER,
         allowNull: false,
+        validate: {
+          isInt: { msg: "Utilisez uniquement des nombres entiers." },
+          notNull: { msg: "Les points de vie sont une propriété requise." },
+        },
       },
       cp: {
         type: DataTypes.INTEGER,
         allowNull: false,
+        validate: {
+          isInt: { msg: "Utilisez uniquement des nombres entiers." },
+          notNull: { msg: "Les points de vie sont une propriété requise." },
+        },
       },
       picture: {
         type: DataTypes.STRING,
         allowNull: false,
+        validate: {
+          isUrl: { msg: "Le chemin vers l'image doit être une url." },
+          notNull: { msg: "Le chemin vers l'image est une propriété requise." },
+        },
       },
       types: {
         type: DataTypes.STRING,
