a0976ee0d6dfbba582042794a45854a29c63d5a3
marcsegard <marcsegard@free.fr>
2021-12-09 17:14:25 +0100

    Ajout validateur perso et contraintes sequelize

--- src/models/pokemon.js
+++ src/models/pokemon.js
@@ -1,3 +1,15 @@
+const validTypes = [
+  "Plante",
+  "Poisson",
+  "Feu",
+  "Eau",
+  "Insecte",
+  "Vol",
+  "Normal",
+  "Electrik",
+  "Fée",
+];
+
 module.exports = (sequelize, DataTypes) => {
   return sequelize.define(
     "Pokemon",
@@ -10,6 +22,9 @@ module.exports = (sequelize, DataTypes) => {
       name: {
         type: DataTypes.STRING,
         allowNull: false,
+        unique: {
+          msg: "Le nom est déjà pris"
+        },
         validate: {
           notEmpty: {
             msg: "Le nom du pokemon ne peut pas être une chaine vide.",
@@ -17,20 +32,32 @@ module.exports = (sequelize, DataTypes) => {
           notNull: { msg: "Le nom du pokemon est une propriété requise." },
         },
       },
+      //entre 0 et 999
       hp: {
         type: DataTypes.INTEGER,
         allowNull: false,
         validate: {
           isInt: { msg: "Utilisez uniquement des nombres entiers." },
           notNull: { msg: "Les points de vie sont une propriété requise." },
+          min: { args: [0], msg: "La valeur min des points de vie  est 0" },
+          max: {
+            args: [999],
+            msg: "La valeur max des points de vie est de 99",
+          },
         },
       },
+      //entre 0 et 99
       cp: {
         type: DataTypes.INTEGER,
         allowNull: false,
         validate: {
           isInt: { msg: "Utilisez uniquement des nombres entiers." },
           notNull: { msg: "Les points de vie sont une propriété requise." },
+          min: { args: [0], msg: "La valeur min des points de dégâts  est 0" },
+          max: {
+            args: [99],
+            msg: "La valeur max des points de dégâts est de 99",
+          },
         },
       },
       picture: {
@@ -49,6 +76,23 @@ module.exports = (sequelize, DataTypes) => {
         },
         set(types) {
           this.setDataValue("types", types.join());
+        },
+        validate: {
+          isTypesValid(value) {
+            if (!value) {
+              throw new Error("Un pokemon doit avoir au moins 1 type");
+            }
+            if (value.split(",").length > 3) {
+              throw new Error("Un pokemon ne peut pas avoir plus de 3 types");
+            }
+            value.split(",").forEach((type) => {
+              if (!validTypes.includes(type)) {
+                throw new Error(
+                  `Le type d'un pokemon doit appartenir à la liste suivante : ${validTypes}`
+                );
+              }
+            });
+          },
         },
       },
     },
